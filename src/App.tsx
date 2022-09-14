79a5a2d46e1ab4ba4420dec78a99f1df0ff36cb4
Calvo Carmona, Maria Trinidad <mtcalvo@minsait.com>
2022-09-14 10:38:00 +0200

    carpeta

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './components/Menu';
+import Menu from './components/navegacion/Menu';
 import Cabecera from './components/Cabecera';
 import PiePagina from './components/PiePagina';
 import ContenidoPaginaPrincipal from './components/ContenidoPaginaPrincipal';
